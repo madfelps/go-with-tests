c214e2dc1d6da8fc4d640b1a5efee16b5517d696
Felipe Madureira <femadureira7@gmail.com>
2024-01-30 11:45:15 -0300

    tests: hello world tests

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/madfelps/go-with-tests
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	prefix := englishHelloPrefix
+
+	switch language {
+	case "French":
+		prefix = frenchHelloPrefix
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	}
+
+	return prefix + name
+}
+
+func main() {
+
+}
